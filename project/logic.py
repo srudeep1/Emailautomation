6a22de9d8cbe68995f69dcda19c1183ca6365d2c
srudeep1 <srudeep.9@gmail.com>
2020-05-21 13:23:13 +0530

    Update ways of sending mails
    - changed message.yml template to take all
      types of content and number of reciepents.
    - changed the code structure to conside all
      cases from the message.yml template

--- project/logic.py
+++ project/logic.py
@@ -13,24 +13,65 @@ def run():
     s.ehlo()
     s.starttls()
     s.login("srudeep.9@gmail.com","qltzilpmbfjgqctb")
-    From,To,Subject,Message=fetch_message_details()
-    msg=message(From,To,Subject,Message)
-    s.send_message(msg)
+    send_message(s)
 
-def fetch_message_details():
+def send_message(smtobject):
     with open('config/message.yml') as f:
         data=yaml.load(f, Loader=yaml.FullLoader)
-    for i in data['data']:
+    for i in data:
+        From,Toname,Subject,Message=fetch_message_details(data,i)
+        validate=validate_contacts(Toname)
+        if validate is not True:
+            exit()
+        condetails=fetch_contact_details(Toname)
+        for i in condetails:
+           key=list(i.keys())[0]
+           value=i.get('{}'.format(key))
+           tempmessage=Message.format(key)
+           msg=message(From,value,Subject,tempmessage)
+           smtobject.send_message(msg)
+
+def validate_contacts(Toname):
+    contactnames=[]
+    with open('contacts/contacts.yml') as f:
+       contacts=yaml.load(f, Loader=yaml.FullLoader)
+    for j in contacts['contacts']:
+       key=list(j.keys())[0]
+       contactnames.append(key)
+    print (contactnames)
+    for i in Toname:
+       if i not in contactnames:
+          msg="The user {} is not found in contacts/contacts.yml. " \
+               "Please Enter correct user name in config/message.yml " \
+              "and re-run".format(i)
+          return msg
+       else:
+          continue
+    return True
+
+def fetch_contact_details(Toname):
+    condetails=[]
+    with open('contacts/contacts.yml') as f:
+        contacts=yaml.load(f, Loader=yaml.FullLoader)
+    for i in Toname:
+        for j in contacts['contacts']:
+            if i in j:
+               condetails.append(j)
+    return condetails
+
+def fetch_message_details(data,j):
+    for i in data['{}'.format(j)]:
        if 'FROM' in i:
            From=i['FROM']
-       if 'TO' in i:
-           To=i['TO']
+       if 'TONAME' in i:
+           To=i['TONAME']
+           To=To.split(',')
        if 'SUBJECT' in i:
            Subject=i['SUBJECT']
        if 'MESSAGE' in i:
            Message=i['MESSAGE']
     return From,To,Subject,Message
-
+         
 
 def message(From,To,Subject,Message):
     msg = MIMEMultipart()
